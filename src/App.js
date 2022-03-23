ff5ddda5e635e266b40e505c38a4b7c294469b16
mashhisyam <haryoh48@gmail.com>
2022-03-23 22:20:05 +0700

    React-route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,334 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+// =================================== NOMOR 1 =================================== 
+
+// export default function BasicExample(){
+//   return(
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/about">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/dashboard">Dashboard</Link>
+//           </li>
+//         </ul>
+//         <hr/>
+//         <Switch>
+//           <Route exact path="/">
+//             <Home />
+//           </Route>
+//           <Route path="/about">
+//             <About />
+//           </Route>
+//           <Route>
+//             <Dashboard />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+// =================================== NOMOR 1 =================================== 
+
+// =================================== NOMOR 2 =================================== 
+
+// export default function ParamsExample(){
+//   return(
+//     <Router>
+//       <div>
+//         <h2>
+//           Accounts
+//         </h2>
+//         <ul>
+//           <li>
+//             <Link to ="/netflix">Netflix</Link>
+//           </li>
+//           <li>
+//             <Link to ="/gmail">Gmail</Link>
+//           </li>
+//           <li>
+//             <Link to ="/yahoo">Yahoo</Link>
+//           </li>
+//           <li>
+//             <Link to ="/amazon">Amazon</Link>
+//           </li>
+//         </ul>
+//         <Switch>
+//           <Route path="/:id" children={<Child />} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+// =================================== NOMOR 2 =================================== 
+
+// =================================== NOMOR 3 =================================== 
+// export default function NestingExample(){
+//   return(
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/topics">Topics</Link>
+//           </li>
+//         </ul>
+//         <hr/>
+//         <Switch>
+//           <Route exact path="/">
+//             <Home />
+//           </Route>
+//           <Route path="/topics">
+//             <Topics />
+//           </Route>
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// }
+// =================================== NOMOR 3 =================================== 
+
+// =================================== NOMOR 4 =================================== 
+export default function AuthExample(){
+  return(
+    <Router>
+      <div>
+        <AuthButton/>
+        <ul>
+          <li>
+            <Link to = "/public">Public Page</Link>
+          </li>
+          <li>
+            <Link to = "/private">Private Page</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/public">
+            <PublicPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <PrivateRoute path="/private">
+            <ProtectedPage />
+          </PrivateRoute>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+const fakeAuth={
+  isAuthenticated: false,
+  authenticate(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100);
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb,100);
+  }
+};
+
+function AuthButton(){
+  let history = useHistory();
+
+  return fakeAuth.isAuthenticated ? (
+    <p>
+    Welcome!{" "}
+    <button
+      onClick={() => {
+        fakeAuth.signout(() => history.push("/"));
+      }}
+    >
+      Sign Out
+    </button>  
+    </p>
+   
+    
+  ) : (
+    <p>
+    You Are Not Logged In.
+    </p>
   );
 }
 
-export default App;
+function PrivateRoute({ children, ...rest}){
+  return(
+    <Route
+      {...rest}
+      render={({ location }) =>
+        fakeAuth.isAuthenticated ? (
+          children
+        ) : (
+          <Redirect
+            to = {{
+              pathname: "/login",
+              state: { from : location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function PublicPage(){
+  return <h3>Public</h3>;
+}
+
+function ProtectedPage(){
+  return <h3>Private</h3>
+}
+
+function LoginPage(){
+  let history = useHistory();
+  let location = useLocation();
+
+  let{ from } = location.state || { from: { pathname: "/"} };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    })
+  };
+  return (
+    <div>
+      <p>You must log in to view the page at {from.pathname}</p>
+      <button onClick={login}>Log in</button>
+    </div>
+  );
+}
+// =================================== NOMOR 4 =================================== 
+
+// =================================== TUGAS ===================================
+
+// =================================== TUGAS ===================================
+
+// =================================== NOMOR 1 =================================== 
+
+// function Home(){
+//   return(
+//     <div>
+//       <h2>
+//         Home
+//       </h2>
+//     </div>
+//   );
+// }
+
+// function About(){
+//   return(
+//     <div>
+//       <h2>
+//         About
+//       </h2>
+//     </div>
+//   );
+// }
+
+// function Dashboard(){
+//   return(
+//     <div>
+//       <h2>
+//         Dashboard
+//       </h2>
+//     </div>
+//   );
+// }
+
+// =================================== NOMOR 1 =================================== 
+
+// =================================== NOMOR 2 =================================== 
+
+// function Child(){
+//   let { id } = useParams();
+
+//   return(
+//     <div>
+//       <h3>
+//         ID: {id}
+//       </h3>
+//     </div>
+//   );
+// }
+
+// =================================== NOMOR 2 =================================== 
+
+// =================================== NOMOR 3 =================================== 
+// function Home(){
+//   return(
+//     <div>
+//       <h2>
+//         Home
+//       </h2>
+//     </div>
+//   );
+// }
+
+// function Topics(){
+//   let {path, url} = useRouteMatch();
+//   return(
+//     <div>
+//       <h2>
+//         Topics
+//       </h2>
+//       <ul>
+//         <li>
+//           <Link to = {`${url}/Sate, Nasi Goreng`}>Kuliner</Link>
+//         </li>
+//         <li>
+//           <Link to = {`${url}/Wisata Alam, Museum`}>Travelling</Link>
+//         </li>
+//         <li>
+//           <Link to = {`${url}/Ibis, JW Marriot`}>Review Hotel</Link>
+//         </li>
+//       </ul>
+
+//       <Switch>
+//         <Route exact path={path}>
+//           <h3>
+//             Please Select a Topic.
+//           </h3>
+//         </Route>
+//         <Route path={`${path}/:topicId`}>
+//           <Topic />
+//         </Route>
+//       </Switch>
+//     </div>
+//   );
+// }
+
+// function Topic(){
+//   let{topicId} = useParams();
+//   return(
+//     <div>
+//       <h3>
+//         {topicId}
+//       </h3>
+//     </div>
+//   );
+// }
+// =================================== NOMOR 3 =================================== 
+
+// =================================== NOMOR 4 =================================== 
+
+// =================================== NOMOR 4 ===================================
